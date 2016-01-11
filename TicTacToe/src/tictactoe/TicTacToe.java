100ad95676fad5f0c77d0c24e803e4716884f825
Anirudh Jain <sponde25@gmail.com>
2016-01-11 20:00:04 +0530

    Point class to store points and PointsAndScores class to store scores of corresponding points.

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tictactoe;
 
-/**
- *
- * @author ASUS
- */
-public class TicTacToe {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class Point {
+
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    
+
+    @Override
+    public String toString() {
+        return "[" + x + ", " + y + "]";
+    }
+}
+
+class PointsAndScores {
+
+    int score;
+    Point point;
+
+    PointsAndScores(int score, Point point) {
+        this.score = score;
+        this.point = point;
+    }
 }
